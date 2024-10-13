e99764d2c8c9b7031229f9142436bff622c2e614
BanuKhasanboeva <hbanu2617@gmail.com>
2024-10-13 16:05:11 +0300

    Initial commit

---
+++ src/UData/InvalidData.java
@@ -0,0 +1,8 @@
+package UData;
+
+public class InvalidData {
+    public static final String userName="";
+    public static final String password="";
+    public static final String patientName="";
+    public static final String patientId="";
+}
---
+++ src/UData/userData.java
@@ -0,0 +1,9 @@
+package UData;
+
+public class userData {
+    public static final String userName="";
+    public static final String password="";
+    public static final String Name=" ";
+    public static final String Id="";
+
+}
