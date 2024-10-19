b06e59b413e37dc9554402a3b45f52f8601fec8d
yukselyucesan <yukselyucesan99@hotmail.com>
2024-10-19 22:58:51 +0300

    Us08 completed.

--- src/US08/us08.java
+++ src/US08/us08.java
@@ -1,4 +1,20 @@
 package US08;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+import static Utility.BaseDriver.driver;
+
 public class us08 {
+    @Test
+    public void TermsOfUse() {
+
+        us08Elements elements = new us08Elements();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, 2000);");
+
+        elements.bizeUlasin.click();
+        elements.okudumKabul.click();
+        elements.kullanimSartlari.click();
+
+    }
 }
