85180cbc53189fc910c2fd90101f7f7fe4ee32b8
BanuKhasanboeva <hbanu2617@gmail.com>
2024-10-20 22:14:00 +0300

    Update us06.java

--- src/US06/us06.java
+++ src/US06/us06.java
@@ -1,19 +1,5 @@
 package US06;
 
 public class us06 {
-  public class us1 extends BaseDriverParameter {
-
-    @Test
-    public void TechnoTest()
-    {
-        driver.get("https://techno.study/tr/");
-
-        WebElement TechnoLogo=driver.findElement(By.xpath("//a[@href='/tr']"));
-        TechnoLogo.click();
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/tr']")));
-
-        Assert.assertTrue(driver.getCurrentUrl().equals("https://techno.study/tr"));
-
-    }
+  
 }
