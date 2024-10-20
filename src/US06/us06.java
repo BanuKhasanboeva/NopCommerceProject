380b874745f137e4684c70a40719f845b399c877
BanuKhasanboeva <hbanu2617@gmail.com>
2024-10-20 23:38:40 +0300

    Update us06.java

--- src/US06/us06.java
+++ src/US06/us06.java
@@ -1,5 +1,20 @@
 package US06;
 
 public class us06 {
+  @Test
+    public void DropDownMenu() {
+        PageHome ph = new PageHome();
+
+        String currentUrl=driver.getCurrentUrl();
+        System.out.println("currentUrl = " + currentUrl);
+        for (int i = 0; i < ph.allLinks.size(); i++) {
+            ph.allLinks = driver.findElements(By.xpath("//a"));
+            if (!ph.allLinks.get(i).getText().equals("Campus Login") && ph.allLinks.get(i).isDisplayed()){
+                System.out.println("ph.allLinks.get(i).getText() = " + ph.allLinks.get(i).getText());
+                ph.myClick(ph.allLinks.get(i));}
+            ph.myClick(ph.logoLink);
+            ph.locationVerification(ph.applyButton);
+        }
+
   
 }
